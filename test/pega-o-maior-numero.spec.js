0d77ff7322c02d01e0dceef71581017560e46f81
mrbrunelli <matheus.brunelli@gmail.com>
2021-12-17 22:02:44 -0300

    test: ensure pegaOMaiorNumero throws Error if provided parameter is not a list

--- test/pega-o-maior-numero.spec.js
+++ test/pega-o-maior-numero.spec.js
@@ -1,10 +1,18 @@
 const assert = require("assert");
 
-const pegaOMaiorNumero = (numeros) => Math.max(...numeros);
+const pegaOMaiorNumero = (numeros) => {
+  if (!Array.isArray(numeros)) return new Error();
+  return Math.max(...numeros);
+};
 
 describe("pegaOMaiorNumero", () => {
   it("Deve retornar o maior número de uma lista", () => {
     const maiorNumero = pegaOMaiorNumero([1, 9, 7, 21, 15, 13]);
     assert.equal(maiorNumero, 21);
+  });
+
+  it("Deve retornar uma exceção caso o parâmetro fornecido não seja uma lista", () => {
+    const maiorNumero = pegaOMaiorNumero("invalid_parameter");
+    assert.deepEqual(maiorNumero, new Error());
   });
 });
